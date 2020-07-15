a5d35ef975201d5365fcc72ab0122fc25ac1c054
danigore <danigoredev@gmail.com>
2020-07-15 10:25:01 +0200

    Basic bootstrap view of posts (on the app home)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <b-button v-if="!postsLoaded" variant="secondary" disabled>
+            <b-spinner small type="grow"></b-spinner>
+            Posts loading...
+        </b-button>
+
+        <b-card-group v-else-if="postsLoaded && posts.length > 0" deck>
+            <b-card v-for="post in posts" :key="post.id" :title="post.title">
+                <b-card-text>
+                    {{ post.content }}
+                </b-card-text>
+
+                <div v-if="post.tags.length">
+                    <b-link  v-for="tag in post.tags" :key="tag.id" :href="'/tag/'+tag.slug" class="card-link">{{ tag.title }}</b-link>
+                </div>
+
+                <template v-slot:footer>
+                    <small class="text-muted">Last updated: {{ post.updated.date }}</small>
+                </template>
+            </b-card>
+        </b-card-group>
+
+        <b-alert v-else-if="postsLoaded && postError.length > 0" show variant="danger">{{ postError }}</b-alert>
+
+        <h1 v-else>Oh no, there is no posts yet ... 😢</h1>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostsMixin from '@/mixins/crud/Posts.mixin'
+import { BCard  } from 'bootstrap-vue'
 
 export default {
   name: 'Home',
+  mixins: [PostsMixin],
   components: {
-    HelloWorld
-  }
+    BCard
+  },
+  created: function () {
+      this.getAllPosts()
+  },
 }
 </script>
+
+<style>
+    .btn-secondary {
+        margin-top: 20px;
+    }
+</style>
