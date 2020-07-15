1af81068120d497231428d7a83fdcb94ee31c427
danigore <danigoredev@gmail.com>
2020-07-15 10:51:56 +0200

    Basic component for Post (PostSkeleton)

---
+++ src/components/post/Skeleton.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-card :title="post.title">
+        <b-card-text>
+            {{ post.content }}
+        </b-card-text>
+
+        <div v-if="post.tags.length">
+            <b-link  v-for="tag in post.tags" :key="tag.id" :href="'/tag/'+tag.slug" class="card-link">{{ tag.title }}</b-link>
+        </div>
+
+        <template v-slot:footer>
+            <small class="text-muted">Last updated: {{ post.updated.date }}</small>
+        </template>
+    </b-card>
+</template>
+
+<script>
+import { BCard  } from 'bootstrap-vue'
+
+export default {
+  name: "PostSkeleton",
+  components: {
+    BCard
+  },
+  props: {
+    post: {
+      type: Array
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,19 +6,7 @@
         </b-button>
 
         <b-card-group v-else-if="postsLoaded && posts.length > 0" deck>
-            <b-card v-for="post in posts" :key="post.id" :title="post.title">
-                <b-card-text>
-                    {{ post.content }}
-                </b-card-text>
-
-                <div v-if="post.tags.length">
-                    <b-link  v-for="tag in post.tags" :key="tag.id" :href="'/tag/'+tag.slug" class="card-link">{{ tag.title }}</b-link>
-                </div>
-
-                <template v-slot:footer>
-                    <small class="text-muted">Last updated: {{ post.updated.date }}</small>
-                </template>
-            </b-card>
+            <post-skeleton v-for="post in posts" :key="post.id" :post="post"></post-skeleton>
         </b-card-group>
 
         <b-alert v-else-if="postsLoaded && postError.length > 0" show variant="danger">{{ postError }}</b-alert>
@@ -30,13 +18,13 @@
 <script>
 // @ is an alias to /src
 import PostsMixin from '@/mixins/crud/Posts.mixin'
-import { BCard  } from 'bootstrap-vue'
+const PostSkeleton = () => import('@/components/post/Skeleton')
 
 export default {
   name: 'Home',
   mixins: [PostsMixin],
   components: {
-    BCard
+    PostSkeleton
   },
   created: function () {
       this.getAllPosts()
